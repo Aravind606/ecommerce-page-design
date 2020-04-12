a096207f844e7c7c3b7dd0952559fa05c9af03e1
Aravind606 <aravind606@yahoo.com>
2020-04-12 13:52:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// jquery(document).ready(function () {
+//   jquery('.baricon').click(function () {
+//     jquery('.sidebar').toggleClass('active')
+//   })
+// });
+
+$('.product-wrapper').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  //autoplay: true,
+  //autoplaySpeed: 2000,
+  nextArrow: $('.next'),
+  prevArrow: $('.prev'),
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
